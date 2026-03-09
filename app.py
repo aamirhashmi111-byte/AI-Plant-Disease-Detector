e132c88ae1c023391791efd0f448ba6cbe08bbde
Aamir khan <aamirhashmi5751@gmail.com>
2026-03-09 11:56:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,36 @@ import cv2
 
 st.title("🌿 AI Plant Disease Detector")
 
-uploaded_file = st.file_uploader(
-    "Upload a leaf image", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload a leaf image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Leaf", width=300)
 
-# prediction
+    # Prediction
     result = predict(uploaded_file)
-
-    st.success(f"Prediction: {result['prediction']} ({result['confidence']}%)")
-
     disease_name = result["prediction"]
+
+    # Grad-CAM
+    img_array = np.array(image.resize((224,224))) / 255.0
+    img_tensor = torch.tensor(img_array).permute(2,0,1).unsqueeze(0).float()
+
+    cam = generate_gradcam(model, img_tensor)
+
+    heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
+    overlay = heatmap * 0.4 + img_array * 255
+
+    # 🔹 Create two columns
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image(image, caption="Uploaded Leaf", width=300)
+
+    with col2:
+        st.image(overlay.astype(np.uint8), caption="Disease Heatmap", width=300)
+
+    # Prediction result
+    st.success(f"Prediction: {result['prediction']} ({result['confidence']}%)")
 
     if disease_name in disease_info:
 
@@ -33,14 +49,3 @@ if uploaded_file is not None:
 
         st.write("Treatment Suggestion:")
         st.write(disease_info[disease_name]["treatment"])
-
-# Grad-CAM
-    img_array = np.array(image.resize((224, 224))) / 255.0
-    img_tensor = torch.tensor(img_array).permute(2, 0, 1).unsqueeze(0).float()
-
-    cam = generate_gradcam(model, img_tensor)
-
-    heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
-    overlay = heatmap * 0.4 + img_array * 255
-
-    st.image(overlay.astype(np.uint8), caption="Disease Heatmap")
